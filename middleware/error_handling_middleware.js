6b8efe3ef13cf45511b8caf319f16a4239988f10
Dilnoza <imomova06@gmail.com>
2023-07-07 04:55:02 +0500

    updated

--- middleware/error_handling_middleware.js
+++ middleware/error_handling_middleware.js
@@ -1,4 +1,5 @@
 const ApiError = require("../error/ApiError")
+
 module.exports = function (err, req, res, next) {
     if (err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message });
@@ -7,10 +8,6 @@ module.exports = function (err, req, res, next) {
     if (err instanceof SyntaxError) {
         return res.status(err.status).json({ message: err.message });
     }
-
-    // if (err.message.includes("Unexpected string in JSON")) {
-    //     return res.status(err.status).json({ message: err.message });
-    // }
 
     return res.status(500).json({ message: "Nazarda tutilmagan xatolik" });
 };
